c2eb32ea7e43f4e15b6f34399a8de1396e4cc6f8
Matt Grobis <matt.grobis@gmail.com>
2018-01-16 22:36:37 +0100

    Update Rot_v_Pol.R

--- Rot_v_Pol.R
+++ Rot_v_Pol.R
@@ -1,7 +1,7 @@
 ######################################################################################################
 # Calculate the global rotation and polarization, and label the group state. Based on Couzin et al.
 # 2002 (Journal of Theoretical Biology) and Tunstrom et al. 2013 (PLoS Computational Biology).
-# Matt Grobis | Oct 2017
+# Matt Grobis | Jan 2018
 #
 # Global rotation, polarization functions:
 # - Inputs: matrices, where each row is an individual's time series for x- and y-coordinates and
@@ -12,79 +12,85 @@
 # - Input: global rotation and polarization vectors
 # - Output: vector of group labels
 #
+#--------------------------------------------------------------------------------
+# calculate_polarization
+# - Definition: how aligned is the group? 0 = no alignment (everyone facing diff 
+#   directions), 1 = perfectly aligned (everyone facing the same direction)
+#
+# - Inputs: the x- and y-unit matrices of the individuals' orientations. 
+#    o Rows = individuals
+#    o Columns = time points
+#
+#-----------------------------------------------------------------------------------------
+# calculate_rotation
+# - Definition: the group's degree of rotation around its center of mass. 
+#   0 = no rotation, 1 = everyone swimming in a big torus
+#
+# - Inputs: the x- and y-position matrices, and the x- and y-unit vectors of orientation.
+#    o Rows = individuals
+#    o Columns = time points
+#
+#-----------------------------------------------------------------------------------------
+# identify_state
+# - Definition: assign the group state
+#    o Rotation < 0.35, Polarization < 0.35: swarm
+#    o Rotation < 0.35, Polarization > 0.65: polarized
+#    o Rotation > 0.65, Polarization < 0.35: milling
+#    o All others: transition
+#
 ######################################################################################################
 
-  calculate_polarization <- function(BO.x, BO.y){
-      #--------------------------------------------------------------------------------
-      # Polarization
-      # - Definition: how aligned is the group? 0 = no alignment (everyone facing diff 
-      #   directions), 1 = perfectly aligned (everyone facing the same direction)
-      #
-      # - Inputs: the x- and y-unit matrices of the individuals' orientations. 
-      #    o Rows = individuals
-      #    o Columns = time points
-      # ------------------------------------------------------------------------------
-      # Calculate the absolute value of the mean individual heading
-      
-          # Step 1: Get mean heading in x direction, mean in y direction
-          # - You want to take all the negative (left/down) components, all the right (right/up)
-          # components, let them accumulate and cancel each other out, and divide what remains by N
-          head.x <- colMeans(BO.x, na.rm = T)
-          head.x[head.x == "NaN"] <- NA
-          
-          head.y <- colMeans(BO.y, na.rm = T)
-          head.y[head.y == "NaN"] <- NA
-          
-          # Step 2: find the magnitude of the resulting vector. 
-          # - This magnitude will be between zero and 1. 0 = all the individuals' headings canceled 
-          #   each other out. 1 = all the individuals' headings were in the same exact direction.
+  calculate_polarization <- function(BO.x, BO.y){   
+      # Step 1: Get mean heading in x direction, mean in y direction
+      # - You want to take all the negative (left/down) components, all the right (right/up)
+      # components, let them accumulate and cancel each other out, and divide what remains by N
+      head.x <- colMeans(BO.x, na.rm = T)
+      head.x[head.x == "NaN"] <- NA
 
-          return(sqrt(head.x^2 + head.y^2))
+      head.y <- colMeans(BO.y, na.rm = T)
+      head.y[head.y == "NaN"] <- NA
+
+      # Step 2: find the magnitude of the resulting vector. 
+      # - This magnitude will be between zero and 1. 0 = all the individuals' headings canceled 
+      #   each other out. 1 = all the individuals' headings were in the same exact direction.
+
+      return(sqrt(head.x^2 + head.y^2))
 
   }
 
   #################################################################################################
   
   calculate_rotation <- function(xs, ys, BO.X, BO.y){
-    #-----------------------------------------------------------------------------------------
-    # Rotation
-    # - Definition: the group's degree of rotation around its center of mass. 
-    #   0 = no rotation, 1 = everyone swimming in a big torus
-    #
-    # - Inputs: the x- and y-position matrices, and the x- and y-unit vectors of orientation.
-    #    o Rows = individuals
-    #    o Columns = time points
-    #-----------------------------------------------------------------------------------------
-    # 1. Calculate the centroid of the group
-    centX <- colMeans(xs, na.rm = T)
-    centX[centX == "NaN"] <- NA
-    
-    centY <- colMeans(ys, na.rm = T)
-    centY[centY == "NaN"] <- NA
-    
-    #--------------------------------------------------------------------------------
-    # 2. Find the distance to centroid
-    dis.x <- sweep(xs, 2, centX, "-")  # Position relative to centroid in x domain
-    dis.y <- sweep(ys, 2, centY, "-")  # Position relative to centroid in y domain
-    
-    tot.dist <- sqrt(dis.x^2 + dis.y^2)
-    
-    #--------------------------------------------------------------------------------
-    # 3. Find relative unit vector from centroid towards each fish
-    rel.u.x <- rel.u.y <- matrix(NA, nrow = nrow(xs), ncol = ncol(xs))
-    rel.u.x <- dis.x / tot.dist
-    rel.u.y <- dis.y / tot.dist
-    
-    #--------------------------------------------------------------------------------------
-    # Group rotation
-    rot <- BO.x*rel.u.y - BO.y*rel.u.x
-    
-    return(abs(colMeans(rot, na.rm = T)))
-    
+      # 1. Calculate the centroid of the group
+      centX <- colMeans(xs, na.rm = T)
+      centX[centX == "NaN"] <- NA
+
+      centY <- colMeans(ys, na.rm = T)
+      centY[centY == "NaN"] <- NA
+
+      #--------------------------------------------------------------------------------
+      # 2. Find the distance to centroid
+      dis.x <- sweep(xs, 2, centX, "-")  # Position relative to centroid in x domain
+      dis.y <- sweep(ys, 2, centY, "-")  # Position relative to centroid in y domain
+
+      tot.dist <- sqrt(dis.x^2 + dis.y^2)
+
+      #--------------------------------------------------------------------------------
+      # 3. Find relative unit vector from centroid towards each fish
+      rel.u.x <- rel.u.y <- matrix(NA, nrow = nrow(xs), ncol = ncol(xs))
+      rel.u.x <- dis.x / tot.dist
+      rel.u.y <- dis.y / tot.dist
+
+      #--------------------------------------------------------------------------------------
+      # Group rotation
+      rot <- BO.x*rel.u.y - BO.y*rel.u.x
+
+      return(abs(colMeans(rot, na.rm = T)))
+
   }
 
   ######################################################################################################
-  # Bonus: classify the group state based on the global rotation and polarization.
+  # Classify the group state based on the global rotation and polarization.
   # - Swarm: rotation < 0.35 & polarization < 0.35
   # - Milling: rotation > 0.65 & polarization < 0.35
   # - Polarized: rotation < 0.35 & polarization > 0.65
